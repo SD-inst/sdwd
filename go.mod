603cf323c1dd8c755628f574e5d9d80e3622fd9c
rkfg <rkfg@rkfg.me>
2023-07-27 20:33:04 +0300

    Add restart on OOM (memleak workaround)

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/rkfg/sdwd
 
 go 1.19
 
-require github.com/coreos/go-systemd/v22 v22.5.0
+require (
+	github.com/coreos/go-systemd/v22 v22.5.0
+	github.com/jessevdk/go-flags v1.5.0
+)
 
-require github.com/godbus/dbus/v5 v5.0.4 // indirect
+require (
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/coreos/go-systemd/v22/dbus"
 	"github.com/coreos/go-systemd/v22/sdjournal"
+	"github.com/jessevdk/go-flags"
 )
 
 const sdServiceName = "sd.service"
 
+var params struct {
+	DockerDir   string `short:"d" description:"Main directory with docker-compose.yml" required:"true"`
+	ServiceName string `short:"s" description:"Stable diffusion docker-compose service name to watch and restart" required:"true"`
+}
+
+func restarter(dockerDir string) chan string {
+	svcChan := make(chan string, 10)
+	go func() {
+		for serviceName := range svcChan {
+			restartCmd := exec.Command("docker", "compose", "restart", serviceName, "-t", "0")
+			restartCmd.Dir = dockerDir
+			restartCmd.Run()
+			log.Printf("Service %s restarted", serviceName)
+		}
+	}()
+	return svcChan
+}
+
+func watchOOM(dockerDir string, serviceName string, restarter chan string) {
+	for {
+		logCmd := exec.Command("docker", "compose", "logs", serviceName, "-n", "1", "-f")
+		logCmd.Dir = dockerDir
+		logPipe, err := logCmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := bufio.NewScanner(logPipe)
+		logCmd.Start()
+		for s.Scan() {
+			line := s.Text()
+			if strings.Contains(line, "torch.cuda.OutOfMemoryError") {
+				log.Println("Stable Diffusion OOM'd, restarting...")
+				restarter <- serviceName
+			}
+		}
+		logCmd.Wait()
+		time.Sleep(time.Second * 5)
+	}
+}
+
 func main() {
+	_, err := flags.Parse(&params)
+	if err != nil {
+		os.Exit(1)
+	}
+	restarterChan := restarter(params.DockerDir)
+	go watchOOM(params.DockerDir, params.ServiceName, restarterChan)
 	conn, err := dbus.NewSystemConnectionContext(context.Background())
 	if err != nil {
 		log.Fatal(err)
@@ -48,10 +99,7 @@ func main() {
 			} else {
 				if state.Value.Value() == "active" {
 					log.Println("sd unit is active, restarting")
-					_, err := conn.RestartUnitContext(context.Background(), sdServiceName, "replace", nil)
-					if err != nil {
-						log.Println("Error restarting service:", err)
-					}
+					restarterChan <- params.ServiceName
 				} else {
 					log.Println("Invalid sd unit state, ignoring:", state.Value.Value())
 				}
